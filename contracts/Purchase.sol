3393a76fc049ea049e86297a906f38cfd0e7d94e
chonghaitao <chonghaitao@gmail.com>
2017-08-25 22:48:26 +0800

    deploy contract

--- contracts/Purchase.sol
+++ contracts/Purchase.sol
@@ -2,5 +2,55 @@ pragma solidity ^0.4.11;
 
 
 contract Purchase {
+    uint public value;
+    address public buyer;
+    address public seller;
+    enum State { Created, Locked, Inactive }
+    State public state;
+    //mapping (address => uint) fromMap;
 
+
+    event PurchaseConfirmed();
+    event ItemReceived();
+    event Aborted();
+
+    modifier onlyBuyer {
+        require(msg.sender == buyer);
+        _;
+    }
+
+    modifier onlySeller {
+        require(msg.sender == seller);
+        _;
+    }
+
+    function Purchase() payable {
+        seller = msg.sender;
+        value = msg.value / 2;
+        state = State.Created;
+        require ((2 * value) == msg.value);
+    }
+
+    function confirmPurchase() payable {
+        require(state == State.Created);
+        require(msg.value == (2*value));
+        buyer = msg.sender;
+        state = State.Locked;
+        PurchaseConfirmed();
+    }
+
+    function confirmReceived() onlyBuyer payable {
+        require(state == State.Locked);
+        state = State.Inactive;
+        ItemReceived();
+        buyer.transfer(value);
+        seller.transfer(this.balance);
+    }
+
+    function abort() onlySeller payable {
+        require(state == State.Created);
+        state = State.Inactive;
+        Aborted();
+        seller.transfer(this.balance);
+    }
 }
--- truffle.js
+++ truffle.js
@@ -1,8 +1,8 @@
-// Following is used for Production Release, uncomment when deployment
-// var WalletProvider = require("truffle-wallet-provider");
-// var keystore = require('fs').readFileSync('./keystore').toString();
-// var pass = require('fs').readFileSync('./pass').toString();
-// var wallet = require('ethereumjs-wallet').fromV3(keystore, pass);
+//Following is used for Production Release, uncomment when deployment
+var WalletProvider = require("truffle-wallet-provider");
+var keystore = require('fs').readFileSync('./keystore').toString();
+var pass = require('fs').readFileSync('./pass').toString();
+var wallet = require('ethereumjs-wallet').fromV3(keystore, pass, true);
 
 module.exports = {
   networks: {
@@ -11,10 +11,10 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     },
-    // ropsten: {
-    //     network_id: 1,
-    //     provider: new WalletProvider(wallet, "http://45.76.197.72:8545"),
-    // }
+    ropsten: {
+         network_id: 1,
+         provider: new WalletProvider(wallet, "http://45.76.197.72:8545"),
+    }
   }
 };
 
